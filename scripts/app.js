f2db6ce8d7a48e67e6aec06de1eb78495a0993a5
SidneyNemzer <sidney@ucode.com>
2016-12-24 17:27:48 -0800

    Begin creation of acheivements list

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,45 @@
+// Data
+const classes = [
+  {
+    name: "General",
+    heros: [
+      {
+        name: "General",
+        image: "",
+        achievements: [
+          {
+            name: "Blackjack",
+            image: "",
+            description: "Earn 21 postgame cards in quick or competative play"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: "Offense",
+    heros: [
+      {
+        name: "Gengi",
+        image: "",
+        achievements: [
+          {
+            name: "Their Own Worst Enemey",
+            description: "Kill 2 enemies with a single use of Gengi's Deflect in quick or competative play"
+          }
+        ]
+      }
+    ]
+  }
+]
+
 // Initialize app
-var myApp = new Framework7();
- 
+var app = new Framework7();
+
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
- 
+
 // Add view
-var mainView = myApp.addView('.view-main', {
-  // Because we want to use dynamic navbar, we need to enable it for this view:
+var mainView = app.addView('.view-main', {
   dynamicNavbar: true
 });
