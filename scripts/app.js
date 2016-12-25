70cacc73f3b4b9b1b23d37f580f3c37c9fe8e40b
SidneyNemzer <sidney@ucode.com>
2016-12-24 18:15:12 -0800

    Add hero list generator

--- scripts/app.js
+++ scripts/app.js
@@ -43,3 +43,34 @@ var $$ = Dom7;
 var mainView = app.addView('.view-main', {
   dynamicNavbar: true
 });
+
+const $listWrapper = document.querySelector('#list-wrapper')
+
+for (let _class = 0; _class<classes.length; _class++) {
+  const curClass = classes[_class]
+
+  $listWrapper.innerHTML += `<div class="content-block-title">`+curClass.name+`</div>
+<div class="list-block media-list inset">
+  <ul>`
+
+  for (let hero = 0; hero < curClass.heros.length; hero++) {
+    const curHero = curClass.heros[hero]
+
+    $listWrapper.innerHTML += `<li>
+    <a href=`+"#"+` class="item-link item-content">
+      <div class="item-media"><img src="`+curHero.image+`" width="44"></div>
+      <div class="item-inner">
+        <div class="item-title-row">
+          <div class="item-title">`+curHero.name+`</div>
+        </div>
+        <div class="item-subtitle">`+curHero.achievements.length+` achievements</div>
+      </div>
+    </a>
+  </li>`
+  }
+
+  $listWrapper.innerHTML += `</ul>
+  </div>`
+}
+$listWrapper.style.display = ''
+document.querySelector('#loading').style.display = 'none'
